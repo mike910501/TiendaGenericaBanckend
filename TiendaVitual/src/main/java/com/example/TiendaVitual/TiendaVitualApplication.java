669004c0ea7bbe8dd2bbc1ebe656b03c71943e38
Mike <Mike@192.168.0.63>
2021-09-26 11:18:27 -0500

    Cargue nuevo sin errores de server

---
+++ TiendaVitual/src/main/java/com/example/TiendaVitual/TiendaVitualApplication.java
@@ -0,0 +1,13 @@
+package com.example.TiendaVitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVitualApplication.class, args);
+	}
+
+}
---
+++ TiendaVitual/src/test/java/com/example/TiendaVitual/TiendaVitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TiendaVitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
